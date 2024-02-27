f95a377c9aa94f32245793e5e9649cf0c229af8b
BernardoCameron <b.cameron.i26@gmail.com>
2024-02-27 16:58:29 -0300

    creando componentes Card y Form

---
+++ src/Components/Form.jsx
@@ -0,0 +1,17 @@
+
+
+export const Form = () => {
+    return (
+        <div>
+
+            <form>
+                <label>Nombre: </label>
+                <input type="text" />
+                <label>Apellido: </label>
+                <input type="text" />
+
+            </form>
+
+        </div>
+    )
+}
